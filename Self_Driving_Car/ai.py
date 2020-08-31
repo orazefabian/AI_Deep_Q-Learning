5573fa1bad5d63ef031f43f5e5636fd89aeb653d
Fabian Oraze <oraze.fabian@icloud.com>
2020-08-31 20:34:00 +0200

    added learn method where the network computes the outputs and the cost function and backpropagates them to adapt the weights

--- Self_Driving_Car/ai.py
+++ Self_Driving_Car/ai.py
@@ -70,3 +70,11 @@ class Dqn:
         action = probs.multinomial(num_samples=1)
         return action.data[0, 0]
 
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        next_outputs = self.model(batch_next_state).detach().max(1)[0]
+        target = self.gamma * next_outputs + batch_reward
+        td_loss = F.smooth_l1_loss(outputs, target)  # best cost function for q-learning
+        self.optimizer.zero_grad()
+        td_loss.backward(retain_graph=True)
+        self.optimizer.step()  # update weights with this step
