89404a0b7901040f796ecab0fab14094521709f6
Fabian Oraze <oraze.fabian@icloud.com>
2020-08-31 14:42:42 +0200

    added init function for network

--- Self_Driving_Car/ai.py
+++ Self_Driving_Car/ai.py
@@ -1,14 +1,27 @@
 # AI for self driving car
 
 # import all libraries
+from typing import Any
 
 import numpy as np
 import random
 import os
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
 
+
+class Network(nn.Module):
+
+    def __init__(self, input_size, nb_actions):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_actions = nb_actions
+        self.fc1 = nn.Linear(input_size, 30)  # second parameter is the amount of hidden layer nodes
+        self.fc2 = nn.Linear(30, nb_actions)  # fc2 is connection between hidden layer and output layer
+
+    def _forward_unimplemented(self, *input: Any) -> None:
+        pass
