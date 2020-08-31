13d1809c33721c4495afe6969ba9dc57f19504f9
Fabian Oraze <oraze.fabian@icloud.com>
2020-08-31 14:26:55 +0200

    initial commit, enviromnent for car already done in kivy

---
+++ Self_Driving_Car/ai.py
@@ -0,0 +1,14 @@
+# AI for self driving car
+
+# import all libraries
+
+import numpy as np
+import random
+import os
+import torch
+import torch.nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
